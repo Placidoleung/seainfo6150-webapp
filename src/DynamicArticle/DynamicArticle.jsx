27ba04184db5ac8d61b413421ba2c55118184c93
iClickOn <clickonliang@gmail.com>
2021-02-26 12:07:35 -0800

    updated DynamicArticle.jsx and App.js

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -5,6 +5,15 @@ const DynamicArticle = (props) => {
   return (
     <article>
       {/* add extra html with javascript attributes here */}
+      <header>
+        <h1>{props.article.title}</h1>
+        <address>
+          by {props.article.author}(
+          <a href={"mailto:"+ props.article.authorEmail}>{props.article.authorEmail}</a>)
+        </address>
+        <time dateTime={props.article.timeStamp}>{props.article.displayDate}</time>
+        <br />
+      </header>
       <HTMLText text={props.article.text} />
     </article>
   );
